3c8a82ff58c1aa55db6de9c6a1c272a39edb4351
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-01-10 22:12:00 -0500

    created MovieCard MoviView and MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,19 +1,58 @@
 import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([
 
-        {id: 1, title: "Death Becomes Her"},
-        {id: 2, title:"Back to the Future"},
-        {id: 3, title: "Up"}
+        {
+            id: 1,
+            title: "Death Becomes Her",
+            image: "https://www.imdb.com/title/tt0104070/mediaviewer/rm106180097/?ref_=tt_ov_i",
+            genre: "Drama",
+            director: "Robert Zemeckis",
+
+        },
+        {
+            id: 2,
+            title: "Back to the Future",
+            image: "https://www.imdb.com/title/tt0088763/mediaviewer/rm554638848/?ref_=tt_ov_i",
+            genre: "High-Concept Comedy",
+            director: "Robert Zemeckis"
+        },
+        {
+            id: 3,
+            title: "Up",
+            image: "https://www.imdb.com/title/tt1049413/mediaviewer/rm3826338560/?ref_=tt_ov_i",
+            genre: "Adventure",
+            director: "Peter Docter"
+        }
     ]);
 
-   return (
-    <div>
-        {movies.map((movie) => {
-            return <div>{movie.title}</div>;
-        })}
-    </div>
-   );
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-};
+    if (selectedMovie) {
+        return (
+            <MoviewView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => {
+                <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            })}
+        </div>
+    );
+
+}
