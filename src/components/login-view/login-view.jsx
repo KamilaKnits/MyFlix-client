dff04defe110f164b505db43014c952fa7b87e61
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-02-08 22:44:49 -0500

    Update login-view.jsx
    changed hears to read application/json

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -17,7 +17,7 @@ export const LoginView= ({ onLoggedIn}) => {
         fetch("https://mymovieflix-a3c1af20a30e.herokuapp.com/login", {
             method: "POST",
             headers: {
-                "Content-Type": "appliication/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         })
